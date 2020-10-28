aa09e3186b9d96cced539b6175d39c2eb3c612a5
mfederi <federici.marco.94@gmail.com>
2020-10-28 12:37:59 +0100

    Dependency fix

--- models/__init__.py
+++ models/__init__.py
@@ -1,5 +1,1 @@
-from models.IDA_ACE import IDAACETrainer
-from models.IDA_MIACE import IDAMIACETrainer
-from models.CIC import CICTrainer
-from models.CIC_MI import CICMITrainer
-from models.JIC_MI import JICMITrainer, PJICMITrainer
+from models.ECI import ECITrainer
