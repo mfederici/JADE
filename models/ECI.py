b71851e6fb59b36be3ae5e32e6b78f4da4787226
mfederi <federici.marco.94@gmail.com>
2020-10-28 11:52:07 +0100

    Cleanup and experiments update

--- models/ECI.py
+++ models/ECI.py
@@ -5,29 +5,31 @@ import torch
 import utils.schedulers as scheduler_module
 
 
-#########################################
-# IDA Adversarial Cross Entropy Trainer #
-#########################################
+###############################################
+# Environment Conditional Independent Trainer #
+###############################################
 
 ADV_ALT_TRAIN = 'alternating'
 ADV_SIM_TRAIN = 'simultaneous'
 
+ADV_CE_LOSS = 'CE'
+ADV_JDS_LOSS = 'JSD'
+
 ADV_TRAIN_TYPES = {ADV_SIM_TRAIN, ADV_ALT_TRAIN}
+ADV_OBJECTIVES = {ADV_CE_LOSS, ADV_JDS_LOSS}
 
 
-class IDAMIACETrainer(RepresentationTrainer):
-    def __init__(self, z_dim, classifier, env_classifier, fea_extractor, mi_estimator, optim, beta_scheduler, n_adv_steps=5, adv_optim=None,
-                 adv_train_type=ADV_ALT_TRAIN, **params):
+class ECITrainer(RepresentationTrainer):
+    def __init__(self, z_dim, classifier, env_classifier, optim, beta_scheduler, n_adv_steps=5, adv_optim=None,
+                 adv_objective=ADV_CE_LOSS, adv_train_type=ADV_ALT_TRAIN, **params):
 
-        super(IDAMIACETrainer, self).__init__(z_dim=z_dim, optim=optim, **params)
+        super(ECITrainer, self).__init__(z_dim=z_dim, optim=optim, **params)
 
         # Definition of the scheduler to update the value of the regularization coefficient beta over time
         self.beta_scheduler = getattr(scheduler_module, beta_scheduler['class'])(**beta_scheduler['params'])
 
         self.classifier = self.instantiate_architecture(classifier, z_dim=z_dim)
         self.env_classifier = self.instantiate_architecture(env_classifier, z_dim=z_dim)
-        self.feature_extractor = self.instantiate_architecture(fea_extractor)
-        self.mi_estimator = self.instantiate_architecture(mi_estimator, size1=z_dim, size2=fea_extractor['params']['f_dim'])
 
         self.n_adv_steps = n_adv_steps
         self.step = 0
@@ -36,20 +38,15 @@ class IDAMIACETrainer(RepresentationTrainer):
             {'params': self.classifier.parameters()}
         )
 
-        self.opt.add_param_group(
-            {'params': self.feature_extractor.parameters()}
-        )
-
-        self.opt.add_param_group(
-            {'params': self.mi_estimator.parameters()}
-        )
+        assert adv_objective in ADV_OBJECTIVES
+        self.adv_objective = adv_objective
 
         assert adv_train_type in ADV_TRAIN_TYPES
         assert adv_train_type != ADV_ALT_TRAIN or not (adv_optim is None)
-
         self.adv_train_type = adv_train_type
 
-        if adv_optim:
+        if adv_train_type == ADV_ALT_TRAIN:
+            assert not (adv_optim is None)
             self.adv_opt = self.instantiate_optimizer(adv_optim, params=self.env_classifier.parameters())
         else:
             self.opt.add_param_group(
@@ -58,12 +55,11 @@ class IDAMIACETrainer(RepresentationTrainer):
             self.adv_opt = None
 
     def _get_items_to_store(self):
-        items_to_store = super(IDAMIACETrainer, self)._get_items_to_store()
+        items_to_store = super(ECITrainer, self)._get_items_to_store()
 
         items_to_store = items_to_store.union({
             'classifier',
-            'env_classifier',
-            'feature_extractor'
+            'env_classifier'
         })
         if self.adv_opt:
             items_to_store = items_to_store.union({'adv_opt'})
@@ -106,28 +102,26 @@ class IDAMIACETrainer(RepresentationTrainer):
         # Encode a batch of data
         z = self.encoder(x=x).rsample()
 
-        # Label Reconstruction (the gradient is not backpropagated through z)
-        p_y_given_z = self.classifier(z=z.detach())
+        # Label Reconstruction
+        p_y_given_z = self.classifier(z=ScaleGrad.apply(z, 1-beta))
+
         y_rec_loss = - p_y_given_z.log_prob(y).mean()
 
-        # Feature recostruction
-        f = self.feature_extractor(x=x)
-        mi_zf, mi_zf_val = self.mi_estimator(ScaleGrad.apply(z, 1 - beta), f)
+        if self.adv_objective == ADV_CE_LOSS:
+            p_e_given_zy = self.env_classifier(z=ScaleGrad.apply(z, -beta), y=y)
+            e_rec_loss = -p_e_given_zy.log_prob(e).mean()
+            self._add_loss_item('loss/CE_e_yz', e_rec_loss.item())
+        else:
+            raise NotImplemented()
 
-        p_e_given_zy = self.env_classifier(z=ScaleGrad.apply(z, -beta), y=y)
-        e_rec_loss = -p_e_given_zy.log_prob(e).mean()
+        loss = y_rec_loss + e_rec_loss
 
-        loss = y_rec_loss + e_rec_loss - mi_zf
-
-        self._add_loss_item('loss/CE_e_yz', e_rec_loss.item())
-        self._add_loss_item('loss/I_z_x', mi_zf_val.item())
         self._add_loss_item('loss/CE_y_z', y_rec_loss.item())
         self._add_loss_item('loss/beta', beta)
 
         return loss
 
     def _compute_adv_loss(self, data):
-        # Read the two views v1 and v2 and ignore the label y
         x = data['x']
         y = data['y']
         e = data['e']
@@ -136,15 +130,9 @@ class IDAMIACETrainer(RepresentationTrainer):
         with torch.no_grad():
             z = self.encoder(x=x).sample()
 
-        # # Label Reconstruction
-        # p_y_given_z = self.classifier(z.detach())
-        # y_rec_loss = - p_y_given_z.log_prob(y).mean()
-
         p_e_given_zy = self.env_classifier(z=z.detach(), y=y)
         e_rec_loss = -p_e_given_zy.log_prob(e).mean()
         self._add_loss_item('loss/CE_e_yz', e_rec_loss.item())
-
-        #self._add_loss_item('loss/CE_y_z', y_rec_loss.item())
 
         loss = e_rec_loss #+ y_rec_loss
         return loss
