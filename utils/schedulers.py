de2698a00dcce4ae36802282ffc1d379df6b4733
mfederi <federici.marco.94@gmail.com>
2020-11-04 10:06:14 +0100

    Implementation of VIB and EI training

--- utils/schedulers.py
+++ utils/schedulers.py
@@ -8,7 +8,7 @@ class Scheduler:
 
 
 class ConstantScheduler(Scheduler):
-    def __init__(self, value, **params):
+    def __init__(self, value):
         self.value = value
 
     def __call__(self, iteration):
