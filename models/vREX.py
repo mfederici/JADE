10ca61d4ca645310effdedee718ea7e736df46ce
mfederi <federici.marco.94@gmail.com>
2021-01-04 17:45:21 +0100

    VREx use std, updated schedule

--- models/vREX.py
+++ models/vREX.py
@@ -10,10 +10,10 @@ import utils.schedulers as scheduler_module
 # http://arxiv.org/abs/2003.00688
 
 class VRExTrainer(RegularizedClassifierTrainer):
-    def __init__(self, **params):
+    def __init__(self, use_std=False, **params):
         super(VRExTrainer, self).__init__(**params)
         n_envs = getattr(self.arch_module, 'N_ENVS')
-
+        self.use_std = use_std
         self.one_hot = OneHot(n_envs)
 
     def _compute_reg_loss(self, data, z):
@@ -33,7 +33,13 @@ class VRExTrainer(RegularizedClassifierTrainer):
         env_loss = (y_rec_loss.unsqueeze(1) * one_hot_e).sum(0)
         env_loss[e_sum > 0] = env_loss[e_sum > 0] / e_sum[e_sum > 0]
         loss_variance = ((env_loss - env_loss[e_sum > 0].mean()) ** 2)[e_sum > 0].mean()
-
         self._add_loss_item('loss/V_CE_y_z', loss_variance.item())
 
-        return loss_variance
+        if self.use_std:
+            loss = loss_variance ** 0.5
+        else:
+            loss = loss_variance
+
+
+
+        return loss
