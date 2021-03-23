57b57bfa705c8d3834074eb1e78bc6952ef449a1
mfederi <federici.marco.94@gmail.com>
2021-03-23 11:47:48 +0100

    IRM fix

--- models/IRM.py
+++ models/IRM.py
@@ -16,21 +16,26 @@ import utils.schedulers as scheduler_module
 class IRMTrainer(RegularizedClassifierTrainer):
     # See https://github.com/facebookresearch/DomainBed/blob/master/domainbed/algorithms.py
 
-    def _compute_irm_loss(self, logits, y):
+    def _compute_irm_loss(self, logits, y, e):
         device = logits.device
         scale = torch.tensor(1.).to(device).requires_grad_()
-        loss_1 = F.cross_entropy(logits[::2] * scale, y[::2].long())
-        loss_2 = F.cross_entropy(logits[1::2] * scale, y[1::2].long())
-        grad_1 = autograd.grad(loss_1, [scale], create_graph=True)[0]
-        grad_2 = autograd.grad(loss_2, [scale], create_graph=True)[0]
+        penalty = 0
+        for i in range(2):
+            logits_e = logits[e == i]
+            y_e = y[e == i]
+            loss_1 = F.cross_entropy(logits_e[::2] * scale, y_e[::2].long())
+            loss_2 = F.cross_entropy(logits_e[1::2] * scale, y_e[1::2].long())
+            grad_1 = autograd.grad(loss_1, [scale], create_graph=True)[0]
+            grad_2 = autograd.grad(loss_2, [scale], create_graph=True)[0]
+            penalty += torch.sum(grad_1 * grad_2)
+        return penalty
 
-        return torch.sum(grad_1 * grad_2)
-
-    def _compute_reg_loss(self, data, z):
+    def _compute_reg_loss(self, data, z, **params):
         y = data['y'].squeeze()
+        e = data['e'].squeeze()
 
         p_y_given_z = self.classifier(z=z)
-        penalty = self._compute_irm_loss(p_y_given_z.logits, y)
+        penalty = self._compute_irm_loss(p_y_given_z.logits, y, e)
         self._add_loss_item('loss/Gradient_penalty', penalty.item())
 
         return penalty
